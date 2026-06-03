7621c117832f4b440b04b5da883b86ff07abe993
Nicolas A. <zeon_j1@proton.me>
2026-06-02 21:08:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -29,14 +29,6 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 function requireAdmin(req, res, next) {
-    if (req.query.token !== ADMIN_TOKEN) {
-        return res.status(403).send("Forbidden");
-    }
-
-    next();
-}
-
-function requireAdmin(req, res, next) {
     if (req.headers["x-admin-token"] !== ADMIN_TOKEN) {
         return res.status(403).send("Forbidden");
     }
